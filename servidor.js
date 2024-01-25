4f0bf9db9415f71e2639cf1f4f4d13f993ee411e
LoiLoL421 <j.robleslecona@ugto.mx>
2024-01-25 09:27:27 -0600

    inicializamos la conexion a la base de datos de firebase

--- servidor.js
+++ servidor.js
@@ -1,17 +1,19 @@
 const express = require('express')
+import { initializeApp } from "firebase/app";
 const app = express()
 const port = 5001
 
-app.get('/', (req, res) => {
-    res.send('Respuesta de la raíz')
-})
+//conexion a la base de datos
 
-app.get('/contacto', (req, res) => {
-    res.send('Respuesta del puñeton')
-})
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBvzTTm3Z5-fsqoKxtUjFifklDd8KqK2KU",
+  authDomain: "crud-practica-1-207ad.firebaseapp.com",
+  projectId: "crud-practica-1-207ad",
+  storageBucket: "crud-practica-1-207ad.appspot.com",
+  messagingSenderId: "273331647195",
+  appId: "1:273331647195:web:16659c47013d6117d4d2fe"
+};
 
-app.listen(port, () => {
-    console.log('listening server: ', port)
-    console.log(`listening server: , ${port}`)  //esta forma con template strings te permite mexclar variables 
-
-})
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
